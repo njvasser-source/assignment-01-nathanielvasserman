f70fecd18ebe3aff7127b96bd328d000e3ac7b98
nathanielvasserman <njvasser@syr.edu>
2025-09-09 14:47:33 -0400

    testing

--- code/numbers.py
+++ code/numbers.py
@@ -11,4 +11,17 @@ For example, if the input is:
 The output should be:
 {'odd': [3, 5], 'even': [2, 4, 6]}
 '''
-
+numbers = {'odd': [], 'even': []}
+while True:
+    try:
+        n = int(input())
+    except ValueError:
+        print("Please enter a valid integer.")
+        continue
+    if n == 0:
+        break
+    if n % 2 == 0:
+        numbers['even'].append(n)
+    else:
+        numbers['odd'].append(n)
+print(numbers)
